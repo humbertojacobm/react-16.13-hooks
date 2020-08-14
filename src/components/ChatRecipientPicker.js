0ff61530db0fed9d001c4c372bef42e1966e2208
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-08-14 16:25:52 -0500

    how to use customeHooks

---
+++ src/components/ChatRecipientPicker.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import useFriendStatus from './useFriendStatus'
+import Circle from './Circle'
+const friendList = [
+  { id: 1, name: 'Phoebe' },
+  { id: 2, name: 'Rachel' },
+  { id: 3, name: 'Ross' },
+]
+
+function ChatRecipientPicker(){
+  const [recipientID, setRecipientID] = useState(1);
+  const isRecipientOnline = useFriendStatus(recipientID);
+
+  return(
+      <>
+        <Circle color={isRecipientOnline ? 'green' : 'red'} />
+        <select value={recipientID}
+                onChange={e=> setRecipientID(Number(e.target.value))}
+        >
+            {
+                friendList.map(friend => (
+                    <option key={friend.id}
+                            value={friend.id}
+                    >
+                        {friend.name}
+                    </option>
+                ))
+            }
+        </select>
+      </>
+  )
+
+}
+
+export default ChatRecipientPicker;
---
+++ src/components/Circle.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+
+function Circle(props){
+    return(
+        <p>hola</p>
+    )
+}
+
+export default Circle;
